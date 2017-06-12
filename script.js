05f998ee7b68d5bcbd9f5e09e8ea92ff2b5a15ef
Timothy Batty <tbatty7@gmail.com>
2017-06-11 23:52:03 -0400

    format and add verbage to js form.

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-/*function greeting(name) {
-if (name == "") {
-	/*var a = document.createElement("P");
-	var b = document.createTextNode("Greetings person who will not type their name!");
-	a.appendChild(b);
-	document.getElementById("newGreeting")*/
-	/*document.getElementById("newGreeting").innerHTML = "Greetings person who will not type their name!";
 
-}
-else {
-	document.appendChild();
-}
-}*/
 function greeting() {
 
 if (document.getElementById('quickName').value) { document.getElementById('newGreeting').innerHTML = 
-                    document.getElementById("quickName").value;
-                                                        }
-    else {
-      document.getElementById('newGreeting').innerHTML = "No input message.";
-    }}
-    /*function showInput() {
-       if (document.getElementById('user_input').value) { document.getElementById('display').innerHTML = 
-                    document.getElementById("user_input").value;
-                                                        }
-    else {
-      document.getElementById('display').innerHTML = "No input message."
-    }}*/
+  "Greetings " + document.getElementById("quickName").value + ", You are an inspiration to me!  I am honored to have you visit my site!";
+} 
+else {
+      document.getElementById('newGreeting').innerHTML = 
+      "Greetings person who will not type their name!  I too am a lurker at times!";
+    }
+    
+  }
